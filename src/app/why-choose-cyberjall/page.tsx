826b939db078260b6955a75be171a009943fb278
Prithxvhie44 <44prithviraj44@gmail.com>
2025-03-08 00:55:45 +0530

    who  we are section

---
+++ src/app/why-choose-cyberjall/page.tsx
@@ -0,0 +1,11 @@
+import { WhyChooseCyberJall } from '../components/sections/WhyChooseCyberJall';
+import { Navbar } from '../components/Navbar';
+
+export default function WhyChooseCyberJallPage() {
+  return (
+    <main className="min-h-screen bg-gray-900">
+      <Navbar />
+      <WhyChooseCyberJall />
+    </main>
+  );
+} 
