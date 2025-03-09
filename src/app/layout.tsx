983fd49f53db7d171f9923fd46edbb1cc6d08b4b
Mastermind730 <sourav.97032@gmail.com>
2025-03-09 21:49:36 +0530

    small fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
-import { Navbar } from "./components/Navbar";
 import NewNavbar from "./components/NewNavbar";
 import Footer from "./components/Footer";
 
