ae44311932a6a757e0b15cdad9d06a6a7327c3e4
Mastermind730 <sourav.97032@gmail.com>
2025-03-31 23:47:31 +0530

    api for fetching all the companies done

---
+++ src/app/api/company/route.ts
@@ -0,0 +1,24 @@
+import { NextResponse } from "next/server";
+import prisma from "@/lib/prismadb";
+
+export async function GET(req: Request) {
+  try {
+    const companies = await prisma.company.findMany({
+        select:{
+            id:true,
+            company_name:true,
+            logo:true,
+            website:true
+        }
+      
+    });
+
+    return NextResponse.json(companies, { status: 200 });
+  } catch (error) {
+    console.error("Error fetching companies:", error);
+    return NextResponse.json(
+      { error: "Failed to fetch companies" },
+      { status: 500 }
+    );
+  }
+}
