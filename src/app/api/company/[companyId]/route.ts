34efef0640db778fbc69aacde9fe8fcf50890a4f
Mastermind730 <sourav.97032@gmail.com>
2025-03-31 23:27:30 +0530

    getting company for the company profile page done

---
+++ src/app/api/company/[companyId]/route.ts
@@ -0,0 +1,34 @@
+import { NextResponse } from "next/server";
+import prisma from "@/lib/prismadb";
+
+
+interface IParams{
+    companyId:string
+}
+// 67ead40f320d9870dde6569e
+export async function GET(req: Request, { params }: { params: IParams }) {
+    try {
+        const { companyId } = params;
+
+        const company = await prisma.company.findUnique({
+            where: {
+                id: companyId
+            }
+        });
+
+        if (!company) {
+            return NextResponse.json(
+                { error: "Company not found" },
+                { status: 404 }
+            );
+        }
+
+        return NextResponse.json(company, { status: 200 });
+    } catch (error) {
+        console.error("Unknown error occurred:", error);
+        return NextResponse.json(
+            { error: "Internal server error" },
+            { status: 500 }
+        );
+    }
+}
