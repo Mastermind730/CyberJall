1bbb0a84dcd1858df5789dc02409958ed89c471f
Mastermind730 <sourav.97032@gmail.com>
2025-04-18 16:28:34 +0530

    api made for getting logo of the company

---
+++ src/app/api/getCompany/route.ts
@@ -0,0 +1,30 @@
+import { NextResponse } from "next/server";
+import prisma from "@/lib/prismadb";
+
+export async function POST(req:Request){
+    try {
+        const body= await req.json();
+    const {company_name}= body;
+
+
+    const company =await prisma.company.findFirst({
+        where:{
+            company_name:company_name
+        }
+    });
+
+    if(!company){
+        return NextResponse.json("Company not found",{status:404});
+    }
+
+    const company_logo=company.logo;
+
+    return NextResponse.json({"company_logo":company_logo},{status:200});
+    } catch (error:unknown) {
+        return NextResponse.json({"error":error},{status:500})
+    }
+    
+
+
+
+}
