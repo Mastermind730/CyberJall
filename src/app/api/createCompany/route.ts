29f0267a5e5970ac129c6f3281d84c145df46709
Mastermind730 <sourav.97032@gmail.com>
2025-06-24 13:46:39 +0530

    some ts fix

--- src/app/api/createCompany/route.ts
+++ src/app/api/createCompany/route.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import prisma from "@/lib/prismadb";
 import { NextResponse } from "next/server";
