e0e92544204c6b3bb30f693ad59a562de2abb0b9
Mastermind730 <sourav.97032@gmail.com>
2025-08-02 23:02:09 +0530

    import errors fixed

--- src/app/api/customer/settings/password/route.ts
+++ src/app/api/customer/settings/password/route.ts
@@ -1,7 +1,7 @@
 import { type NextRequest, NextResponse } from "next/server"
 import { getServerSession } from "next-auth"
 import { authOptions } from "@/lib/auth"
-import { prisma } from "@/lib/prisma"
+import  prisma  from "@/lib/prismadb"
 import bcrypt from "bcryptjs"
 
 export async function PUT(request: NextRequest) {
